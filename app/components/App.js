c0b78a9aa6ad7c94a913220fc2c8fa29011477c6
Alex Simonian <alex.simonian@gmail.com>
2017-04-16 17:31:45 -0500

    Adding React Router

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,27 @@
 let React = require('react');
 let Popular = require('./Popular');
+let ReactRouter = require('react-router-dom');
+let Router = ReactRouter.BrowserRouter;
+let Route = ReactRouter.Route;
+let Switch = ReactRouter.Switch;
+let Nav = require('./Nav');
+let Home = require('./Home');
+let Battle = require('./Battle');
 
 class App extends React.Component {
 	render() {
 		return (
-			<div className="container">
-				<Popular />
-			</div>
+			<Router>
+				<div className="container">
+					<Nav />
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route path="/popular" component={Popular} />
+						<Route exact path="/battle" component={Battle} />
+						<Route render={ () => <p>Not Found</p> } />
+					</Switch>
+				</div>
+			</Router>
 		)
 	}
 }
---
+++ app/components/Battle.js
@@ -0,0 +1,13 @@
+let React = require('react');
+
+class Battle extends React.Component {
+	render() {
+		return (
+			<div>
+				BATTLE!
+			</div>
+		)
+	}
+}
+
+module.exports = Battle;
